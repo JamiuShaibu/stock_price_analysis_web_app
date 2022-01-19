422b4873b1c33a9a218e4b31a496eb0b19cc16c0
Jamiu Shaibu <jamiushaibu12@gmail.com>
2022-01-19 11:24:02 +0100

    Added a while loop
    Description

--- sp_app/my_app.py
+++ sp_app/my_app.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import date
 from PIL import Image
 
+stl.markdown("<h5 style='text-align: center; color: navy'>WELCOME TO THE STOCK PRICE ANALYTIC WEB APP!</h5>", unsafe_allow_html=True)
 # Display image in the web app
 image = Image.open('global-markets.jpg')
 stl.image(image, use_column_width=True)
@@ -14,10 +15,6 @@ stl.markdown("<h3 style='text-align: center; font:bold'>Simple Stock Price App</
 print("\nWELCOME TO THE STOCK PRICE ANALYTIC WEB APP!\n")
 stock_name = input("Enter Stock Name: ")
 tickerSymbol = stock_name.strip().upper()
-
-stl.write("""
-***
-Shown are the stock **closing price** and ***volume*** of """ + tickerSymbol)
 
 # Get data on this ticker.
 tickerData = yf.Ticker(tickerSymbol)
@@ -30,43 +27,50 @@ start_year = input("\nEnter Starting Year in this format : YY-MM-DD; e.g 2000-1-
 end_year = input("\nEnter Ending Year in this format : YY-MM-DD; e.g 2000-1-1\nEnter Year : ")
 
 # Set Current date as default if ending year is not provided
-if len(end_year) == 0:
-    tickerDf = tickerData.history(period='1d', start=start_year, end=today)
-    stl.write("""Showing """ + tickerSymbol, """Stock Price **from:** """ + start_year, """- Current Date""")
-    stl.write("""**NOTE:** You didn't **enter** Targeted Closing Date. **Current Date is set as Default.**""")
+while len(stock_name) != 0:
+    stl.write("""
+    ***
+    Shown are the stock **closing price** and ***volume*** of """ + tickerSymbol)
 
-elif len(start_year) == 0:
-    # Set Default Starting year
-    default_start = "2000-1-1"
-    tickerDf = tickerData.history(period='1d', start=default_start, end=end_year)
-    stl.write("""Showing """ + tickerSymbol, """Stock Price **from:** """ + default_start, """- """, end_year)
-    stl.write("""**NOTE:** You didn't **enter** Targeted Starting Date. **Starting Date is set to 2000-1-1 as Default.**""")
+    if len(end_year) == 0:
+        tickerDf = tickerData.history(period='1d', start=start_year, end=today)
+        stl.write("""Showing """ + tickerSymbol, """Stock Price **From:** """ + start_year, """**To** Current Date""")
+        stl.write("""**NOTE:** You didn't **enter** Targeted Closing Date. **Current Date is set as Default.**""")
 
-elif len(start_year) == 0 and len(end_year) == 0:
-    # Set Default
-    start_from = "2000-1-1"
-    tickerDf = tickerData.history(period='1d', start=start_from, end=today)
-    stl.write("""Showing """ + tickerSymbol, """Stock Price **from:** """ + start_from, """- Current Date""")
-    stl.write("""**NOTE:** You didn't **enter** Targeted Date for both Start and Close. **2000-1-1 and Current Date are set as Default.**""")
+    elif len(start_year) == 0:
+        # Set Default Starting year
+        default_start = "2000-1-1"
+        tickerDf = tickerData.history(period='1d', start=default_start, end=end_year)
+        stl.write("""Showing """ + tickerSymbol, """Stock Price **From:** """ + default_start, """**To** """, end_year)
+        stl.write("""**NOTE:** You didn't **enter** Targeted Starting Date. **Starting Date is set to 2000-1-1 as Default.**""")
 
-# Provide data within start_year and end_year
+    elif len(start_year) == 0 and len(end_year) == 0:
+        # Set Default
+        start_from = "2000-1-1"
+        tickerDf = tickerData.history(period='1d', start=start_from, end=today)
+        stl.write("""Showing """ + tickerSymbol, """Stock Price **From:** """ + start_from, """**To** Current Date""")
+        stl.write("""**NOTE:** You didn't **enter** Targeted Date for both Start and Close. **2000-1-1 and Current Date are set as Default.**""")
+
+    # Provide data within start_year and end_year
+    else:
+        tickerDf = tickerData.history(period='1d', start=start_year, end=end_year)
+        stl.write("""Showing """ + tickerSymbol, """Stock Price **From:** """ + start_year, """**To**""", end_year)
+
+    # Open   High   Low   Close Volume  Dividends   Stock  Splits.
+    stl.markdown("<h5 style='text-align: left; color: navy'>Closing Price</h5>", unsafe_allow_html=True)
+    stl.line_chart(tickerDf.Close)
+
+    stl.markdown("<h5 style='text-align: left; color: navy'>Volume Price</h5>", unsafe_allow_html=True)
+    stl.line_chart(tickerDf.Volume)
+    # To break the while loop or the app will run forever.
+    break
+
 else:
-    tickerDf = tickerData.history(period='1d', start=start_year, end=end_year)
-    stl.write("""Showing """ + tickerSymbol, """Stock Price **from:** """ + start_year, """-""", end_year)
-# Open   High   Low   Close Volume  Dividends   Stock  Splits.
+    stl.write("""**NOTE:** Stock Name Not Provided. Please **Refresh the Browser** and **Enter Stock Name in command Prompt.**\n""")
 
-stl.markdown("<h5 style='text-align: left; color: navy'>Closing Price</h5>", unsafe_allow_html=True)
-# stl.write("""
-# ## Closing Price
-# """)
-stl.line_chart(tickerDf.Close)
-
-stl.markdown("<h5 style='text-align: left; color: navy'>Volume Price</h5>", unsafe_allow_html=True)
-# stl.write("""
-# ## Volume Price
-# """)
-stl.line_chart(tickerDf.Volume)
-
+stl.write("""
+    *** """)
+# A little info about stocks
 stl.markdown("<h6 style='text-align: center;'>A share price is the price of a single share of a number of saleable equity shares of a company. In layman's terms, the stock price is the highest amount someone is willing to pay for the stock, or the lowest amount that it can be bought for. The price is a reflection of the company’s value – what the public is willing to pay for a piece of the company. It can and will rise and fall, based on a variety of factors in the global landscape and within the company itself.</h6>", unsafe_allow_html=True)
 
 
